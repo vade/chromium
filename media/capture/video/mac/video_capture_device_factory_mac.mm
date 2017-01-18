3407bdb68cf99f6278d0529c3e74f45a4eb67be1
vade <vade@vade.info>
2017-01-18 17:58:24 -0500

    Not quite working but close to working stub for Syphon Servers as seen by the Directory listing, forwarded as available devices from a proxy video capture device.

--- media/capture/video/mac/video_capture_device_factory_mac.mm
+++ media/capture/video/mac/video_capture_device_factory_mac.mm
@@ -18,6 +18,7 @@
 #include "base/task_runner_util.h"
 #import "media/capture/video/mac/video_capture_device_avfoundation_mac.h"
 #import "media/capture/video/mac/video_capture_device_decklink_mac.h"
+#import "media/capture/video/mac/video_capture_device_syphon_mac.h"
 #include "media/capture/video/mac/video_capture_device_mac.h"
 
 namespace media {
@@ -58,7 +59,7 @@ std::unique_ptr<VideoCaptureDevice> VideoCaptureDeviceFactoryMac::CreateDevice(
   if (descriptor.capture_api == VideoCaptureApi::MACOSX_DECKLINK) {
     capture_device.reset(new VideoCaptureDeviceDeckLinkMac(descriptor));
   } else if (descriptor.capture_api == VideoCaptureApi::MACOSX_SYPHON) {
-//      capture_device.reset(new VideoCaptureDeviceDeckLinkMac(descriptor));
+      capture_device.reset(new VideoCaptureDeviceSyphonMac(descriptor));
   } else {
     VideoCaptureDeviceMac* device = new VideoCaptureDeviceMac(descriptor);
     capture_device.reset(device);
